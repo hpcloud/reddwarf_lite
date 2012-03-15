69d82a61c319d030059d801355fe8aaafc5f4c8e
Vipul Sabhaya <vipuls@gmail.com>
2012-03-14 17:56:50 -0700

    change DB model from snapshots to snapshot

--- reddwarf/database/models.py
+++ reddwarf/database/models.py
@@ -236,7 +236,7 @@ class ServiceFlavor(DatabaseModelBase):
     _data_fields = ['service_name', 'flavor_name', 'flavor_id']
 
 
-class Snapshots(DatabaseModelBase):
+class Snapshot(DatabaseModelBase):
     _data_fields = ['instance_id', 'name', 'state', 'user_id', 
                     'tenant_id', 'storage_uri', 'credential', 'storage_size']
     
