35d24fabd3dedebe1233f7697912cf470fc70ffe
Vipul Sabhaya <vipuls@gmail.com>
2012-03-28 15:03:12 -0700

    implement worker_api

---
+++ reddwarf/database/worker_api.py
@@ -0,0 +1,41 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 Hewlett-Packard Development Company, L.P.
+# Copyright (c) 2011 OpenStack, LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Handles all request to the Platform Application Server
+"""
+
+import logging
+
+from reddwarf.database import utils as utils
+from reddwarf.rpc import impl_kombu as rpc
+
+
+LOG = logging.getLogger(__name__)
+
+class API():
+    """API for interacting with the guest manager."""
+
+#    def __init__(self, **kwargs):
+#        super(API, self).__init__(**kwargs)
+
+    def ensure_create_instance(self, context, instance_id):
+        LOG.debug("Triggering worker app server to ensure instance created: %s.", instance_id)
+        instance = utils.get_instance(instance_id)
+        rpc.cast(context, 'work',
+                 {"create-instance":{"id": instance_id}})
